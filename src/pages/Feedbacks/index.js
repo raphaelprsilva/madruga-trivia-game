d3d0a37eeda5108a49da9d031d4db8a14f953a29
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-27 19:24:50 -0300

    feat: add Layout component to feedbacks page

--- src/pages/Feedbacks/index.js
+++ src/pages/Feedbacks/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout';
 
 class Feedbacks extends Component {
   render() {
     return (
-      <div data-testid="feedback-text">
-        <h1>Feedbacks</h1>
-      </div>
+      <Layout>
+        <div data-testid="feedback-text">
+          <h1>Feedbacks</h1>
+        </div>
+      </Layout>
     );
   }
 }
