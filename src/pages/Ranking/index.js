d730fec0d353982783c42a2067ac1cd8371db9c1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-28 09:40:25 -0300

    feat: ensures user is redirected to login page

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,13 +1,30 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <div>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ () => history.push('/') }
+          >
+            Home
+          </button>
+        </div>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
