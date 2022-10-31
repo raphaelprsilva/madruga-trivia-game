e7b5a4e148b11e1470a30614fafad4d1cdb6b3e7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-31 08:52:09 -0300

    style: add Layout component style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Header from '../Header';
+import * as S from './styled';
 
 class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
-      <main>
+      <>
         <Header />
-        {children}
-      </main>
+        <S.MainWrapper>
+          {children}
+        </S.MainWrapper>
+      </>
     );
   }
 }
